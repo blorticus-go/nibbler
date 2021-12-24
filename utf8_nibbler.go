347dd6300b4b60c1a12c69387b07fad4a4220cbf
blorticus <boguese@hotmail.com>
2021-12-23 17:31:28 -0800

    push

--- utf8_nibbler.go
+++ utf8_nibbler.go
@@ -1,8 +1,61 @@
 package nibblers
 
 import (
+	"fmt"
+	"io"
 	"unicode/utf8"
 )
+
+type simpleSizeConstrainedStackOfUints struct {
+	underlyingSlice   []uint
+	indexOfHead       int
+	currentStackDepth int
+	maxStackDepth     int
+}
+
+func newSimpleSizeConstrainedStackOfUints(maximuimStackLength uint) *simpleSizeConstrainedStackOfUints {
+	if maximuimStackLength < 2 {
+		panic("simpleSizeConstrainedStackOfUints length must be at least 2")
+	}
+
+	return &simpleSizeConstrainedStackOfUints{
+		underlyingSlice:   make([]uint, maximuimStackLength),
+		indexOfHead:       -1,
+		maxStackDepth:     int(maximuimStackLength),
+		currentStackDepth: 0,
+	}
+}
+
+func (stack *simpleSizeConstrainedStackOfUints) Push(value uint) {
+	if stack.indexOfHead == stack.maxStackDepth-1 {
+		stack.indexOfHead = 0
+	} else {
+		stack.indexOfHead++
+	}
+
+	stack.underlyingSlice[stack.indexOfHead] = value
+
+	if stack.currentStackDepth < stack.maxStackDepth {
+		stack.currentStackDepth++
+	}
+}
+
+func (stack *simpleSizeConstrainedStackOfUints) Pop() (value uint, stackIsAlreadyEmpty bool) {
+	if stack.currentStackDepth == 0 {
+		return 0, true
+	}
+
+	value = stack.underlyingSlice[stack.indexOfHead]
+	if stack.indexOfHead == 0 {
+		stack.indexOfHead = len(stack.underlyingSlice) - 1
+	} else {
+		stack.indexOfHead--
+	}
+
+	stack.currentStackDepth--
+
+	return value, false
+}
 
 type NamedUnicodeCharacterSetsMap struct {
 	mapOfSetsByName map[string]map[rune]bool
@@ -52,10 +105,30 @@ type UTF8Nibbler interface {
 	ReadNextCharactersNotMatchingSet(setName string) ([]rune, error)
 }
 
-type UTF8StringNibbler struct{}
+type UTF8StringNibbler struct {
+	backingString               string
+	indexInStringOfNextReadByte int
+}
+
+func NewUTF8StringNibbler(nibbleString string) *UTF8StringNibbler {
+	return &UTF8StringNibbler{
+		backingString:               nibbleString,
+		indexInStringOfNextReadByte: 0,
+	}
+}
 
 func (nibbler *UTF8StringNibbler) ReadCaracter() (rune, error) {
-	return 0, nil
+	if nibbler.indexInStringOfNextReadByte >= len(nibbler.backingString) {
+		return 0, io.EOF
+	}
+
+	nextCharacter, sizeOfCharacterInBytes := utf8.DecodeRuneInString(nibbler.backingString[nibbler.indexInStringOfNextReadByte:])
+	if nextCharacter == utf8.RuneError {
+		return 0, fmt.Errorf("Invalid UTF-8 string element")
+	}
+
+	nibbler.indexInStringOfNextReadByte += sizeOfCharacterInBytes
+	return nextCharacter, nil
 }
 
 func (nibbler *UTF8StringNibbler) UnreadCharacter() error {
